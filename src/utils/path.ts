84e21325b5de2651048d2b9ff35bd329cf74d39d
Guisper <3038363597@qq.com>
2022-08-10 23:37:25 +0800

    更改静态资源路径，避免打包后仍使用src路径

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -2,8 +2,8 @@ import { cwd } from 'process'
 import { resolve } from 'path'
 
 // 需要用到的本地静态资源的路径
-const staticPath: string = resolve(cwd(), './src/static')
-const staticPagePath: string = resolve(cwd(), './src/static/index.html')
-const userinfoPath: string = resolve(cwd(), './src/static/userinfo.json')
+const staticPath: string = resolve(cwd(), './static')
+const staticPagePath: string = resolve(cwd(), './static/index.html')
+const userinfoPath: string = resolve(cwd(), './static/userinfo.json')
 
 export { staticPath, staticPagePath, userinfoPath }
