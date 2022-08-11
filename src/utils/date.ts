6f99fbffd072182ea68d1a81e7f3f0fd426aa9c5
Guisper <3038363597@qq.com>
2022-08-11 12:32:48 +0800

    更改获取日期字符串的方式，兼容不同运行环境

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -4,10 +4,15 @@ const currentTime: number = Date.now()
 const suffixes: Array<string> = ['年', '月', '日']
 
 const getDateString = (date?: number): string => new Date(date ?? currentTime).toLocaleDateString(locale)
-const getFormattedString = (): string =>
-  getDateString()
-    .split('/')
+
+// 获取 yyyy年mm月dd日 的日期字符串，兼容不同运行环境
+const getFormattedString = (): string => {
+  const dateString: string = getDateString()
+  const date: Array<Array<string>> = [dateString.split('/'), dateString.split('-')]
+  return date
+    .find(d => d.length === 3)!
     .map((e, i) => e + suffixes[i])
     .join('')
+}
 
 export default getFormattedString
