8fd1031ba757cf6dc851b2dc5846a7293a884aef
Guisper <Guisper@qq.com>
2022-08-11 15:52:12 +0800

    更改页面处理的方式，删除static/style.css

--- src/utils/pageProcessor.ts
+++ src/utils/pageProcessor.ts
@@ -1,9 +1,10 @@
 // 对页面的资源(css js 图片等)路径进行处理
 const pageProcessor = (page: string): string =>
   page
-    .replace(/(?<=\<head>)[\s\S]+?(?=\<\/head>)/, '<link rel="stylesheet" href="./static/style.css">')
-    .replace('../../cdu/sdcms/js12/jquery.min.js','https://cdn.bootcdn.net/ajax/libs/jquery/1.10.2/jquery.min.js')
-    .replace('assets/css/bootstrap.min.css','https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css')
+    .replace(/\<link.+?\/\>/g, '')
+    .replace(/(<script(.*?)>)[\s\S]*?(<\/script>)/g, '')
+    .replace(/(?<=\<head\>)[\s\S]/, '<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">')
+    .replace('../../isp/images/protect.png','https://xsswzx.cdu.edu.cn/isp/images/protect.png')
     .replace('img/yiliao.png', 'https://xsswzx.cdu.edu.cn/ispstu/com_user/img/yiliao.png')
     .replace('images/load.gif', '')
     .replace('img/loading.gif', '')
