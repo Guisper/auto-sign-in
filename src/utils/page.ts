9ed135ba056371d41229479dbf34d45c4c415fd6
Guisper <3038363597@qq.com>
2022-08-13 17:40:33 +0800

    去除页面上不必要的链接和内容

--- src/utils/page.ts
+++ src/utils/page.ts
@@ -2,9 +2,10 @@
 const pageProcessor = (page: string): string =>
   page
     .replace(/\<link.+?\/\>/g, '')
-    .replace(/href="(.+?[^*\.css])"/g,'')
+    .replace(/href=["'](.+?[^*\.css])["']/g,'')
     .replace(/(<script(.*?)>)[\s\S]*?(<\/script>)/g, '')
-    .replace(/(?=\<\/html>)/,'<script>;var d=document,q=d.querySelector.bind(d);q(".card.mb-3").remove();q(".card-header").remove();</script>')
+    .replace(/(<div class="card mb-3">)[\s\S]*?(<div class="">)/,'</div>')
+    .replace(/(<div class="card-header">)[\s\S]*?(\<tr style="display:none;">)/,'<tr>')
     .replace(/(?<=\<head\>)[\s\S]/, '<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">')
     .replace('../../isp/images/protect.png','https://xsswzx.cdu.edu.cn/isp/images/protect.png')
     .replace('img/yiliao.png', 'https://xsswzx.cdu.edu.cn/ispstu/com_user/img/yiliao.png')
