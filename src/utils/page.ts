6ff8c6e87d25c23402404c7c6045b7a924f5ce11
Guisper <3038363597@qq.com>
2022-08-14 17:12:13 +0800

    删除页面上display为none的文字和图片

--- src/utils/page.ts
+++ src/utils/page.ts
@@ -2,15 +2,16 @@
 const pageProcessor = (page: string): string =>
   page
     .replace(/\<link.+?\/\>/g, '')
-    .replace(/href=["'](.+?[^*\.css])["']/g,'')
+    .replace(/href=["'](.+?[^*\.css])["']/g, '')
     .replace(/(<script(.*?)>)[\s\S]*?(<\/script>)/g, '')
-    .replace(/(<div class="card mb-3">)[\s\S]*?(<div class="">)/,'</div>')
-    .replace(/(<div class="card-header">)[\s\S]*?(\<tr style="display:none;">)/,'<tr>')
-    .replace(/(?<=\<head\>)[\s\S]/, '<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">')
-    .replace('../../isp/images/protect.png','https://xsswzx.cdu.edu.cn/isp/images/protect.png')
+    .replace(/(<div class="card mb-3">)[\s\S]*?(<div class="">)/, '</div>')
+    .replace(/(<div class="card-header">)[\s\S]*?(\<tr style="display:none;">)/, '<tr>')
+    .replace(/(?<=\<div class="card mb-3">)[\s\S]*?(?=\<div)/, '')
+    .replace(/<center>[\s\S]*?<\/center>/, '')
+    .replace(/(?<=\<head\>)[\s\S]/, '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">')
+    .replace('../../isp/images/protect.png', 'https://xsswzx.cdu.edu.cn/isp/images/protect.png')
     .replace('img/yiliao.png', 'https://xsswzx.cdu.edu.cn/ispstu/com_user/img/yiliao.png')
     .replace('images/load.gif', '')
-    .replace('img/loading.gif', '')
     .replace('无标题文档', '打卡结果')
-    
+
 export default pageProcessor
