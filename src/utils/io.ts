3711e51f749d6050286abe05971ca49af69792c8
Guisper <guisper@qq.com>
2022-08-21 22:16:30 +0800

    删除io模块的部分输出

--- src/utils/io.ts
+++ src/utils/io.ts
@@ -1,9 +1,8 @@
 import fs from 'fs'
-import { success, info } from './output'
+import { success } from './output'
 
 // 若文件夹不存在则创建
 const checkDir = (path: string): Promise<void> => {
-  info('checkDir: ' + path)
   return new Promise((resolve, reject) => {
     fs.access(path, err => {
       err ? fs.mkdir(path, e => (e ? reject(e) : resolve())) : resolve()
@@ -13,7 +12,6 @@ const checkDir = (path: string): Promise<void> => {
 
 // 用来读文件的，可选择是否解析为 JSON，对一些错误作了处理
 const reader = (path: string, parse: boolean = true): Promise<[boolean, any]> => {
-  info('reader: ' + path)
   return new Promise((resolve, reject) => {
     fs.stat(path, async (_, stat) => {
       if (stat?.isFile()) {
@@ -38,7 +36,6 @@ const reader = (path: string, parse: boolean = true): Promise<[boolean, any]> =>
 
 // 用来写文件的
 const writer = (path: string, data: string): Promise<void> => {
-  info('writer: ' + path)
   return new Promise((resolve, reject) => {
     fs.writeFile(path, data, err => {
       err ? reject('文件写入错误') : resolve(success('文件写入成功'))
