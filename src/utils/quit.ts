ed575549a066b5525b179d484584e8a221afc8d5
Guisper <guisper@qq.com>
2022-08-22 11:27:41 +0800

    自动任务不显示退出倒计时

--- src/utils/quit.ts
+++ src/utils/quit.ts
@@ -1,10 +1,13 @@
 import { stdout } from 'single-line-log'
+const { env: { ACCOUNT }, exit } = process
 
-const second = 1000
+const second: number = 1000
+const quitImmediately: boolean = !!ACCOUNT
 
 const slog = (countdown: number): void => stdout(countdown / second + '秒后自动退出...\n')
 
 const quit = (timer: number = 30000, code: number = 1) => {
+  quitImmediately && exit(code)
   let countdown = timer
   slog(countdown)
   const id = setInterval(() => {
@@ -14,7 +17,7 @@ const quit = (timer: number = 30000, code: number = 1) => {
 
   setTimeout(() => {
     clearInterval(id)
-    process.exit(code)
+    exit(code)
   }, timer)
 }
 
