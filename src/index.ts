6e93c2390bbaff716f982cba830d2205a36005dd
Guisper <guisper@qq.com>
2022-08-25 01:58:58 +0800

    更改非打卡错误和打卡结果的处理方式

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { error } from './utils/output'
+import { error, success, warn } from './utils/output'
 import { loginUrl, idUrl, signInUrl, generalSignInUrl, pageUrl } from './utils/url'
 
 import { checkUserinfo } from './services/userinfoUnit'
@@ -9,21 +9,18 @@ import getPageResult from './services/getPageResult'
 import createServer from './services/createServer'
 import quit from './utils/quit'
 
-!(async () => {
-  try {
-    const [isAutoSignIn, isFirstExec, userinfo] = await checkUserinfo()
-    await login(loginUrl, userinfo, isAutoSignIn)
-    const id = await getUserId(idUrl)
-    await submitRequest({
-      id,
-      userinfo,
-      isFirstExec,
-      urls: [signInUrl, generalSignInUrl]
-    })
-    await getPageResult(pageUrl, id)
-    !isAutoSignIn && await createServer()
-  } catch (e) {
-    error('操作失败，错误信息：', e as string)
+const main = async () => {
+  const [isAutoSignIn,isFirstExec, userinfo] = await checkUserinfo()
+  await login(loginUrl, userinfo, isAutoSignIn)
+  const id = await getUserId(idUrl)
+  await submitRequest(isFirstExec, userinfo, id, signInUrl, generalSignInUrl)
+  await getPageResult(pageUrl, id).catch(e => warn(e))
+  !isAutoSignIn && await createServer().catch(e => warn(e))
+}
+
+main()
+  .then(() => success('今日打卡成功'))
+  .catch(e => {
+    error('操作失败，错误信息：', e)
     quit()
-  }
-})()
+  })
--- src/services/getPageResult.ts
+++ src/services/getPageResult.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { minify } from 'html-minifier'
 
 import { checkDir, writer } from '../utils/io'
-import { info, success, warn } from '../utils/output'
+import { info, success } from '../utils/output'
 import { staticPagePath, staticPath } from '../utils/path'
 import pageProcessor from '../utils/page'
 
@@ -17,19 +17,14 @@ const minifyOptions = {
 
 const getPageResult = async (pageUrl: string, id: string): Promise<void> => {
   // 解析打卡的结果页面，加工和压缩后写入文件
-  try {
-    info('尝试获取打卡页面信息...')
-    const params = { id }
-    const { data } = await axios.get(pageUrl, { params })
-    success('获取成功')
-    info('写入文件...')
-    // 写入前检查文件夹
-    await checkDir(staticPath)
-    await writer(staticPagePath, minify(pageProcessor(data), minifyOptions))
-  } catch (e) {
-    // 在这里捕获错误是为了防止文件读写错误被认为打卡失败
-    warn(e as string)
-  }
+  info('尝试获取打卡页面信息...')
+  const params = { id }
+  const { data } = await axios.get(pageUrl, { params })
+  success('获取成功')
+  info('写入文件...')
+  // 写入前检查文件夹
+  await checkDir(staticPath)
+  await writer(staticPagePath, minify(pageProcessor(data), minifyOptions))
 }
 
 export default getPageResult
